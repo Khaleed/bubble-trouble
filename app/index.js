a32c6ee80bd63c6df200c5282b3cf5c248ea4f06
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-25 00:15:17 +0100

    Delete file

--- app/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// sample code
-
-let numbers = [1, 2, 3, 4, 5];
-// double the list of numbers and filter out the even ones
-console.log(numbers.map(n => n * 2)
-            .filter(n => n % 2 === 0));
-
