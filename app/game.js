c609d8bfdbb2a0e703287a1e1c01d0c7c52b22c1
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-28 02:09:48 +0100

    Remove checking if browser supports canvas and add arrow functions

--- app/game.js
+++ app/game.js
@@ -5,13 +5,10 @@ import assets from "./assets";
 (function () {
     let canvas = assets.canvas;
     window.addEventListener("load", () => {
-        let ctx;
-        if (canvas.getContext === "undefined") {
-            console.error("Browser doesn't support HTML5 Canvas");
-        }
-        ctx = canvas.getContext("2d");
+        const screen = canvas.getContext("2d");
         canvas.width = 1200;
         canvas.height = 800;
+
         const initialGameState = {
             x: canvas.width/2,
             y: canvas.height/2,
@@ -21,35 +18,35 @@ import assets from "./assets";
             radius: 25
         };
 
-        function update(oldState) {
-            console.log("gameState is", oldState.y, canvas.height - oldState.radius,  oldState.vy);
+        const update = oldState => {
             const dt = 0.02;
             const newX = oldState.x + (dt * oldState.vx);
             const newY = oldState.y + (dt * oldState.vy);
-            const doReflectX = newX < oldState.radius || newX > canvas.width - oldState.radius;
+            const doReflescreen = newX < oldState.radius || newX > canvas.width - oldState.radius;
             const doReflectY = newY <  oldState.radius || newY > canvas.height - oldState.radius;
             const newState = {
                 x: newX,
                 y: newY,
-                vx: doReflectX ? (oldState.vx * -1) : oldState.vx,
+                vx: doReflescreen ? (oldState.vx * -1) : oldState.vx,
                 vy: doReflectY ? (oldState.vy * -1) : oldState.vy,
                 color: oldState.color,
                 radius: oldState.radius
             };
             return newState;
-        }
-        function draw(gameState) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.beginPath();
-            ctx.arc(gameState.x, gameState.y, gameState.radius, 0, Math.PI*2, false);
-            ctx.fillStyle = gameState.color;
-            ctx.fill();
-            ctx.closePath();
-        }
-        function runGameRenderingCycle(gameState) {
+        };
+        // impure rendering fn that populates canvas
+        const draw = gameState => {
+            screen.clearRect(0, 0, canvas.width, canvas.height);
+            screen.beginPath();
+            screen.arc(gameState.x, gameState.y, gameState.radius, 0, Math.PI*2, false);
+            screen.fillStyle = gameState.color;
+            screen.fill();
+            screen.closePath();
+        };
+        const runGameRenderingCycle = gameState => {
             draw(gameState);
-            requestAnimationFrame(function(){runGameRenderingCycle(update(gameState));});
-        }
+            requestAnimationFrame(() => runGameRenderingCycle(update(gameState)));
+        };
         runGameRenderingCycle(initialGameState);
     });
 }());
