2a1a72c520dafc15cbd22f67df0bfff3b6a1d7a3
Khalid Omar Ali <khalid.ali@consultant.com>
2016-11-08 22:27:53 +0000

    Refactor the game state data structure for multiple bubbles

--- app/game.js
+++ app/game.js
@@ -8,44 +8,57 @@ import { List, Map } from "immutable";
 
     window.addEventListener("load", () => {
         const screen = canvas.getContext("2d");
-        const initialGameState = Map({
+        const initialGameState = [Map({
             x: canvas.width/2,
             y: canvas.height/2,
             vx: 100,
             vy: 200,
             color: "red",
             radius: 25
-        });
+        }), Map({
+            x: canvas.width/2,
+            y: canvas.height/2,
+            vx: -100,
+            vy: 200,
+            color: "green",
+            radius: 25
+        })];
 
-        function update(oldState) {
+        console.log(initialGameState);
+
+        function updateBubble(oldBubble) {
             const dt = 0.02;
             const g = 200;
-            const newVY = oldState.get("vy") + (g * dt);
-            const newX = oldState.get("x") + (dt * oldState.get("vx"));
-            const newY = oldState.get("y") + (dt * newVY);
-            const doReflectX = newX < oldState.get("radius") || newX > canvas.width - oldState.get("radius");
-            const doReflectY = newY < oldState.get("radius") || newY > canvas.height - oldState.get("radius");
-            const newState = oldState.merge(Map({
+            const newVY = oldBubble.get("vy") + (g * dt);
+            const newX = oldBubble.get("x") + (dt * oldBubble.get("vx"));
+            const newY = oldBubble.get("y") + (dt * newVY);
+            const doReflectX = newX < oldBubble.get("radius") || newX > canvas.width - oldBubble.get("radius");
+            const doReflectY = newY < oldBubble.get("radius") || newY > canvas.height - oldBubble.get("radius");
+            const newBubble = oldBubble.merge(Map({
                 x: newX,
                 y: newY,
-                vx: doReflectX ? (oldState.get("vx") * -1) : oldState.get("vx"),
+                vx: doReflectX ? (oldBubble.get("vx") * -1) : oldBubble.get("vx"),
                 vy: doReflectY ? newVY * -1 : newVY
             }));
-            return newState;
+            return newBubble;
         };
 
+        function updateGame(oldState) {
+            return oldState.map(updateBubble);
+        }
+
         // impure rendering fn that populates canvas
-        function draw(gameState) {
-            screen.clearRect(0, 0, canvas.width, canvas.height);
+        function drawBubble(bubble) {
             screen.beginPath();
-            screen.arc(gameState.get("x"), gameState.get("y"), gameState.get("radius"), 0, Math.PI*2, false);
-            screen.fillStyle = gameState.get("color"); // eslint-disable-line fp/no-mutation
+            screen.arc(bubble.get("x"), bubble.get("y"), bubble.get("radius"), 0, Math.PI*2, false);
+            screen.fillStyle = bubble.get("color"); // eslint-disable-line fp/no-mutation
             screen.fill();
             screen.closePath();
         };
         function runGameRenderingCycle(gameState) {
-            draw(gameState);
-            requestAnimationFrame(() => runGameRenderingCycle(update(gameState)));
+            screen.clearRect(0, 0, canvas.width, canvas.height);
+            gameState.map(drawBubble);
+            requestAnimationFrame(() => runGameRenderingCycle(updateGame(gameState)));
         };
         runGameRenderingCycle(initialGameState);
     });
