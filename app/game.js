33473e3150f2c73ea26502ffc7cc3fb78869cc33
Khalid Omar Ali <khalid.ali@consultant.com>
2016-11-08 23:32:06 +0000

    Add a player in both visually and underlying game state

--- app/game.js
+++ app/game.js
@@ -1,30 +1,37 @@
 /*eslint fp/no-unused-expression: 0, fp/no-nil: 0 */
 
 import assets from "./assets";
+import keys from "./keystate";
 import { List, Map } from "immutable";
 
 (function () {
     const canvas = assets.canvas;
-
+    keys.addListeners();
     window.addEventListener("load", () => {
         const screen = canvas.getContext("2d");
-        const initialGameState = [Map({
-            x: canvas.width/2,
-            y: canvas.height/2,
-            vx: 100,
-            vy: 200,
-            color: "red",
-            radius: 25
-        }), Map({
-            x: canvas.width/2,
-            y: canvas.height/2,
-            vx: -100,
-            vy: 200,
-            color: "green",
-            radius: 25
-        })];
-
-        console.log(initialGameState);
+        const initialGameState = Map({
+            bubbleArray: [Map({
+                x: canvas.width/2,
+                y: canvas.height/2,
+                vx: 100,
+                vy: 200,
+                color: "red",
+                radius: 25
+            }),Map({
+                x: canvas.width/2,
+                y: canvas.height/2,
+                vx: -100,
+                vy: 200,
+                color: "green",
+                radius: 25
+            })],
+            player: Map({
+                x: canvas.width/2,
+                w: 20,
+                h: 40,
+                color: "blue"
+            })
+        });
 
         function updateBubble(oldBubble) {
             const dt = 0.02;
@@ -44,7 +51,17 @@ import { List, Map } from "immutable";
         };
 
         function updateGame(oldState) {
-            return oldState.map(updateBubble);
+            const playerNewX = oldState.get("player").get("x") + (keys.leftPressedKey ? -10: 0 + keys.rightPressedKey ? 10: 0);
+            const newState = Map({
+                bubbleArray: oldState.get("bubbleArray").map(updateBubble),
+                player: oldState.get("player").merge({x: playerNewX})
+            });
+            return newState;
+        }
+
+        function drawPlayer(player) {
+            screen.fillStyle = player.get("color"); // eslint-disable-line fp/no-mutation
+            screen.fillRect(player.get("x"), canvas.height - player.get("h"), player.get("w"), player.get("h"));
         }
 
         // impure rendering fn that populates canvas
@@ -56,8 +73,9 @@ import { List, Map } from "immutable";
             screen.closePath();
         };
         function runGameRenderingCycle(gameState) {
-            screen.clearRect(0, 0, canvas.width, canvas.height);
-            gameState.map(drawBubble);
+            screen.clearRect(0, 0, canvas.width, canvas.height); // clear the screen
+            gameState.get("bubbleArray").map(drawBubble);
+            drawPlayer(gameState.get("player"));
             requestAnimationFrame(() => runGameRenderingCycle(updateGame(gameState)));
         };
         runGameRenderingCycle(initialGameState);
