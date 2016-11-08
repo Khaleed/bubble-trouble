cab1d2b64d23eb8d4c27102e5152be16194a7014
Khalid Omar Ali <khalid.ali@consultant.com>
2016-11-08 23:32:40 +0000

    Detect key state

--- app/keystate.js
+++ app/keystate.js
@@ -1,1 +1,31 @@
-"use strict";
+/*eslint fp/no-unused-expression: 0, fp/no-nil: 0 */
+export default {
+    rightPressedKey: false,
+    leftPressedKey: false,
+    spacePressedKey: false,
+    rPressedKey: false,
+    addListeners: function() {
+        document.addEventListener("keyup", e => {
+            if (e.keyCode === 37) {
+                this.leftPressedKey = false;
+            } else if (e.keyCode === 39) {
+                this.rightPressedKey = false;
+            } else if (e.keyCode === 32) {
+                this.spacePressedKey = false;
+            } else if (e.keyCode === 82) {
+                this.rPressedKey = false;
+            }
+        });
+        document.addEventListener('keydown', e => {
+            if (e.keyCode === 37) {
+                this.leftPressedKey = true;
+            } else if (e.keyCode === 39) {
+                this.rightPressedKey = true;
+            } else if (e.keyCode === 32) {
+                this.spacePressedKey = true;
+            } else if (e.keyCode === 82) {
+                this.rPressedKey = true;
+            }
+        });
+    }
+};
