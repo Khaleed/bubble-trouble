ca41549040b0b8825b6dfe6b56b05d1b377c2bec
Khalid Omar Ali <khalid.ali@consultant.com>
2016-11-26 02:04:40 +0000

    Break GameState function into smaller functions that update the model

--- app/update.js
+++ app/update.js
@@ -1,68 +1,60 @@
-import { Model } from "./model";
 import { List, Map } from "immutable";
+import { default as inputs } from "./assets";
 
-function GameState(args) {
-    const { Inputs, Model } = args;
-
-    function getNewVY(oldBubble, dt, g) {
-        return oldBubble.get("vy") + (g * dt);// newVY = oldVY + acceleration * delta time
-    }
-
-    function getNewX(oldBubble, dt) {
-        return oldBubble.get("x") + (dt * oldBubble.get("vx"));
-    }
-
-    function getNewY(oldBubble, dt, newVY) {
-        return oldBubble.get("y") + (dt * newVY);
-    }
-
-    function doReflectX(newX, oldBubble, canvasWidth) {
-        return newX < oldBubble.get("radius") || newX > canvas.width - oldBubble.get("radius"); // detect collision with left and right canvas border
-    }
-
-    function doReflectY(newY, oldBubble, canvasHeight) {
-        return newY < oldBubble.get("radius") || newY > canvas.height - oldBubble.get("radius"); // detect collision with top and bottom canvas border
-    }
-
-    function updateBubble(oldBubble) {
-        const newVY = getNewVY(oldBubble, 0.02, 200);
-        const newX = getNewX(oldBubble, 0.02);
-        const newY = getNewY(oldBubble, 0.02, newVY);
-        const newBubble = oldBubble.merge(Map({
-            x: newX,
-            y: newY,
-            vx: doReflectX(newX, oldBubble, canvas.width) ? (oldBubble.get("vx") * -1) : oldBubble.get("vx"),
-            vy: doReflectY(newY, oldBubble, canvas.height) ? -500 : newVY
-        }));
-        return newBubble;
-    };
-
-    function updateArrow(oldArrow) {
-        if (oldArrow === null) {
-            return null;
-        }
-        const newY = oldArrow.get("y") - 10;
-        const newArrow = oldArrow.merge({ y: newY });
-        return newY > 0 ? newArrow : null; // return null if there are no arrows
-    }
-
-    function updateGame(oldState) {
-        const player = oldState.get("player");
-        const leftMovement = keys.leftPressedKey && player.get("x") > 0;
-        const rightMovement = keys.rightPressedKey && player.get("x") < (canvas.width - player.get("w"));
-        const playerNewX = player.get("x") + (leftMovement ? -10: 0 + rightMovement ? 10: 0);
-        const newArrowCond = keys.spacePressedKey && oldState.get("arrows").size === 0;
-        const newArrowList1 = newArrowCond ? oldState.get("arrows").push(Map({x: player.get("x") + (player.get("w") / 2) - 1, y: canvas.height, w: 3})) : oldState.get("arrows");
-        const newArrowList2 = newArrowList1.filter((arrow) => arrow !== null);
-        const newState = Map({
-            bubbleArray: oldState.get("bubbleArray").map(updateBubble),
-            player: player.merge({x: playerNewX}),
-            arrows: newArrowList2.map(updateArrow)
-        });
-        return newState;
-    }
-    // if there is no change in game state, return the initial Model
-    return Model;
+function getNewVY(oldBubble, dt, g) {
+    return oldBubble.get("vy") + (g * dt);// newVY = oldVY + acceleration * delta time
 }
 
-export default GameState;
+function getNewX(oldBubble, dt) {
+    return oldBubble.get("x") + (dt * oldBubble.get("vx"));
+}
+
+function getNewY(oldBubble, dt, newVY) {
+    return oldBubble.get("y") + (dt * newVY);
+}
+
+function doReflectX(newX, oldBubble, canvasWidth) {
+    return newX < oldBubble.get("radius") || newX > canvasWidth - oldBubble.get("radius"); // detect collision with left and right canvas border
+}
+
+function doReflectY(newY, oldBubble, canvasHeight) {
+    return newY < oldBubble.get("radius") || newY > canvasHeight - oldBubble.get("radius"); // detect collision with top and bottom canvas border
+}
+
+function updateBubble(oldBubble) {
+    const newVY = getNewVY(oldBubble, 0.02, 200);
+    const newX = getNewX(oldBubble, 0.02);
+    const newY = getNewY(oldBubble, 0.02, newVY);
+    const newBubble = oldBubble.merge(Map({
+        x: newX,
+        y: newY,
+        vx: doReflectX(newX, oldBubble, inputs.canvas.width) ? (oldBubble.get("vx") * -1) : oldBubble.get("vx"),
+        vy: doReflectY(newY, oldBubble, inputs.canvas.height) ? -500 : newVY
+    }));
+    return newBubble;
+};
+
+function updateArrow(oldArrow) {
+    if (oldArrow === null) {
+        return null;
+    }
+    const newY = oldArrow.get("y") - 10;
+    const newArrow = oldArrow.merge({ y: newY });
+    return newY > 0 ? newArrow : null; // return null if there are no arrows -> change this to a mayBe structure
+}
+
+export default function updateGame(oldState, keys, canvas) {
+    const player = oldState.get("player");
+    const leftMovement = keys.leftPressedKey && player.get("x") > 0;
+    const rightMovement = keys.rightPressedKey && player.get("x") < (canvas.width - player.get("w"));
+    const playerNewX = player.get("x") + (leftMovement ? -10: 0 + rightMovement ? 10: 0);
+    const newArrowCond = keys.spacePressedKey && oldState.get("arrows").size === 0;
+    const newArrowList1 = newArrowCond ? oldState.get("arrows").push(Map({x: player.get("x") + (player.get("w") / 2) - 1, y: inputs.canvas.height, w: 3})) : oldState.get("arrows");
+    const newArrowList2 = newArrowList1.filter(arrow => arrow !== null);
+    const newGameState = Map({
+        bubbleArray: oldState.get("bubbleArray").map(updateBubble),
+        player: player.merge({x: playerNewX}),
+        arrows: newArrowList2.map(updateArrow)
+    });
+    return newGameState;
+}
