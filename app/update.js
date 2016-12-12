9c35821e9b8a8adfbb2ec4ae615cc012d9390d29
Khalid Omar Ali <khalid.ali@consultant.com>
2016-12-12 01:00:35 +0000

    Change function expressions to arrow functions

--- app/update.js
+++ app/update.js
@@ -1,25 +1,17 @@
 import { List, Map } from "immutable";
+import { dist } from "./helpers";
 
-function getNewVY(vx, dt, g) {
-    return vx + (g * dt);// newVY = oldVY + acceleration * delta time
-}
+const getNewVY = (vy, dt, g) => vy + (g * dt);// newVY = oldVY + acceleration * delta time
 
-function getNewX(x, dt, vx) {
-    return x + (dt * vx);
-}
+const getNewX = (x, dt, vx) => x + (dt * vx);
 
-function getNewY(y, dt, newVY) {
-    return y + (dt * newVY);
-}
-function doReflectX(newX, radius, canvasWidth) {
-    return newX < radius || newX > canvasWidth - radius; // collision with left and right canvas border
-}
+const getNewY = (y, dt, newVY) => dt * newVY;
 
-function doReflectY(newY, radius, canvasHeight ) {
-    return newY < radius || newY > canvasHeight - radius; // detect collision with top and bottom canvas border
-}
+const doReflectX = (newX, radius, canvasWidth) => newX < radius || newX > (canvasWidth - radius); // collision with left and right canvas border
 
-function updateBubble(bubble) {
+const doReflectY= (newY, radius, canvasHeight) => newY < radius || newY > (canvasHeight - radius); // detect collision with top and bottom canvas border
+
+const updateBubble = bubble => {
     const vX = bubble.get("vx");
     const radius = bubble.get("radius");
     const newVY = getNewVY(bubble.get("vy"), 0.02, 200);
@@ -33,7 +25,7 @@ function updateBubble(bubble) {
     }));
 };
 
-function updateArrow(arrow) {
+const updateArrow = arrow => {
     const step = 10;
     if (arrow === null) {
         return null; // use a mayBe
@@ -41,26 +33,22 @@ function updateArrow(arrow) {
     const newY = arrow.get("y") - step;
     const newArrow = arrow.merge({ y: newY});
     return newY > 0 ? newArrow : null;
-}
+};
 
-function updatePlayerMovement(keys, player, canvasWidth) {
+const updatePlayerMovement = (keys, player, canvasWidth) => {
     const step = 10;
     const playerX = player.get("x");
     const isMovingleft = keys.state.get("isLeftKeyPressed") && playerX > 0;
     const isMovingRight = keys.state.get("isRightKeyPressed") && playerX < (canvasWidth - player.get("w"));
     const deltaInMovement = (isMovingleft ? -step: 0) + (isMovingRight ? step: 0);
     return playerX + deltaInMovement;
-}
+};
 
-function isPlayerShooting(keys, arrows) {
-    return keys.state.get("isSpaceKeyPressed") && arrows.size === 0;
-}
+const isPlayerShooting = (keys, arrows) => keys.state.get("isSpaceKeyPressed") && arrows.size === 0;
 
-function createArrow(keys, arrows, newArrow) {
-    return isPlayerShooting(keys, arrows) ? arrows.push(newArrow) : arrows; // eslint-disable-line fp/no-mutating-methods
-}
+const createArrow = (keys, arrows, newArrow) => isPlayerShooting(keys, arrows) ? arrows.push(newArrow) : arrows; // eslint-disable-line fp/no-mutating-methods
 
-function getNewArrowList(keys, player, arrows, canvasHeight) {
+const getNewArrowList = (keys, player, arrows, canvasHeight) => {
     const YOrigin = canvasHeight - player.get("h");
     const newArrow = Map({
         x: player.get("x") + (player.get("w") / 2) - 1,
--- app/view.js
+++ app/view.js
@@ -3,56 +3,52 @@
 import { default as Html } from "./html";
 import { interrogateKeyState, keys } from "./keystate";
 import { List, Map } from "immutable";
-import updateGame from "./update";
+import { updateGame } from "./update";
 import { Model } from "./model";
 
-(function () {
+interrogateKeyState(keys);
 
+window.addEventListener("load", () => {
     const canvas = Html.canvas;
+    const screen = canvas.getContext("2d");
 
-    interrogateKeyState(keys);
+    const drawPlayer = player => {
+        screen.fillStyle = player.get("color"); // eslint-disable-line fp/no-mutation
+        screen.fillRect(player.get("x"), canvas.height - player.get("h"), player.get("w"), player.get("h"));
+    };
 
-    window.addEventListener("load", () => {
-        const screen = canvas.getContext("2d");
+    const drawBubble = bubble => {
+        screen.beginPath();
+        screen.arc(bubble.get("x"), bubble.get("y"), bubble.get("radius"), 0, Math.PI*2, false);
+        screen.fillStyle = bubble.get("color"); // eslint-disable-line fp/no-mutation
+        screen.fill();
+        screen.closePath();
+    };
 
-        function drawPlayer(player) {
-            screen.fillStyle = player.get("color"); // eslint-disable-line fp/no-mutation
-            screen.fillRect(player.get("x"), canvas.height - player.get("h"), player.get("w"), player.get("h"));
+    const drawArrow = arrow => {
+        if (arrow === null) {
+            return;
         }
+        screen.fillStyle = "white"; // eslint-disable-line fp/no-mutation
+        screen.fillRect(arrow.get("x"), arrow.get("y"), arrow.get("w"), arrow.get("yOrigin") - arrow.get("y"));
+    };
 
-        function drawBubble(bubble) {
-            screen.beginPath();
-            screen.arc(bubble.get("x"), bubble.get("y"), bubble.get("radius"), 0, Math.PI*2, false);
-            screen.fillStyle = bubble.get("color"); // eslint-disable-line fp/no-mutation
-            screen.fill();
-            screen.closePath();
-        };
+    const draw = gameState => {
+        screen.clearRect(0, 0, canvas.width, canvas.height);
+        gameState.get("bubbleArray").map(drawBubble);
+        gameState.get("arrows").map(drawArrow);
+        drawPlayer(gameState.get("player"));
+    };
 
-        function drawArrow(arrow) {
-            if (arrow === null) {
-                return;
-            }
-            screen.fillStyle = "white"; // eslint-disable-line fp/no-mutation
-            screen.fillRect(arrow.get("x"), arrow.get("y"), arrow.get("w"), arrow.get("yOrigin") - arrow.get("y"));
-        }
-
-        function draw(gameState) {
-            screen.clearRect(0, 0, canvas.width, canvas.height);
-            gameState.get("bubbleArray").map(drawBubble);
-            gameState.get("arrows").map(drawArrow);
-            drawPlayer(gameState.get("player"));
-        }
-
-        function runGameRenderingCycle(gameState, lastTime) {
-            const time = new Date().getTime();
-            const deltaInTime = time - (lastTime || time);
-            draw(gameState);
-            requestAnimationFrame(
-                () => runGameRenderingCycle(
-                    updateGame(gameState, keys, canvas.width, canvas.height, deltaInTime), time
-                )
-            );
-        };
-        runGameRenderingCycle(Model, 0);
-    });
-}());
+    const runGameRenderingCycle = (gameState, lastTime) => {
+        const time = new Date().getTime();
+        const deltaInTime = time - (lastTime || time);
+        draw(gameState);
+        requestAnimationFrame(
+            () => runGameRenderingCycle(
+                updateGame(gameState, keys, canvas.width, canvas.height, deltaInTime), time
+            )
+        );
+    };
+    runGameRenderingCycle(Model, 0);
+});
