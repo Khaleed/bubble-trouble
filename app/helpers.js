f25c0c3efe4f759d5271676f05f7c68268bf61fa
Khalid Omar Ali <khalid.ali@consultant.com>
2016-12-12 02:20:12 +0000

    Use Math.hypot for the dist function and add curry and compose

--- app/helpers.js
+++ app/helpers.js
@@ -1,1 +1,15 @@
-export const dist = (x, y) => Math.sqrt(x * x + y * y);
+const dist = (x, y) => Math.hypot(x, y);
+
+const curry = (fn) => {
+    const curried = (f, init) =>
+              (...args) => { // eslint-disable-line fp/no-rest-parameters
+                  const acc = [...init, ...args];
+                  const arity = f.length;
+                  return acc.length > arity ? f(...acc) : curried(f, acc);
+              };
+    return curried(fn, []);
+};
+
+const compose = (...fs) => fs.reduce((f, g) => (...args) => f(g(...args))); // eslint-disable-line fp/no-rest-parameters
+
+export { dist, curry, compose };
