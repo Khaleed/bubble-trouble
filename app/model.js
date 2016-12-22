8dd4f909820d56f90d8fb91f33d7e1ac8bea7ddb
Khalid Omar Ali <khalid.ali@consultant.com>
2016-12-22 22:13:01 +0000

    Update model to include standardised bubbles

--- app/model.js
+++ app/model.js
@@ -3,21 +3,23 @@ import { List, Map } from "immutable";
 
 const canvas = Html.canvas;
 
-export const Model = Map({
+const Model = Map({
     bubbleArray: List.of(Map({
         x: canvas.width/2,
         y: canvas.height/2,
         vx: 100,
         vy: 200,
         color: "red",
-        radius: 25
+        radius: 25,
+        size: 2
     }), Map({
         x: canvas.width/2,
         y: canvas.height/2,
         vx: -100,
         vy: 200,
         color: "green",
-        radius: 25
+        radius: 25,
+        size: 2
     })),
     player: Map({
         x: canvas.width/2,
@@ -27,3 +29,42 @@ export const Model = Map({
     }),
     arrows: List.of()
 });
+
+// default data
+const standardBubbles = List.of(
+    Map({
+        // size === 0
+        vy_init: 200,
+        radius: 10
+    }),
+    Map({
+        // size === 1
+        vy_init: 250,
+        radius: 20
+    }),
+    Map({
+        // size === 2
+        vy_init: 300,
+        radius: 30
+    }),
+    Map({
+        // size === 3
+        vy_init: 350,
+        radius: 45
+    })
+);
+
+const standard_vx = 100;
+
+//construct bubble
+const constructBubble = (x, y, direction_right, color, size) => Map({
+    x: x,
+    y: y,
+    vx: direction_right ? standard_vx : -standard_vx,
+    vy: standardBubbles.get(size).get("vy_init"),
+    color: color,
+    radius: standardBubbles.get(size).get("radius"),
+    size: size
+});
+
+export { Model, constructBubble };
