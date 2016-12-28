511b925938327d4a62cdc6116fc8bd81dd53406d
Khalid Omar Ali <khalid.ali@consultant.com>
2016-12-28 23:25:26 +0000

    Uncomment curry function for future use

--- app/helpers.js
+++ app/helpers.js
@@ -1,14 +1,14 @@
 const dist = (x, y) => Math.hypot(x, y);
 
-// const curry = (fn) => {
-//     const arity = fn.length;
-//     const curried = (f, init) =>
-//               (...args) => { // eslint-disable-line fp/no-rest-parameters
-//                   const acc = [...init, ...args];
-//                   return acc.length > arity ? f(...acc) : curried(f, acc);
-//               };
-//     return curried(fn, []);
-// };
+const curry = (fn) => {
+    const arity = fn.length;
+    const curried = (f, init) =>
+              (...args) => { // eslint-disable-line fp/no-rest-parameters
+                  const acc = [...init, ...args];
+                  return acc.length > arity ? f(...acc) : curried(f, acc);
+              };
+    return curried(fn, []);
+};
 
 const partial = (f, ...init) => (...args) => fn(...init, ...args); // eslint-disable-line fp/no-rest-parameters
 
