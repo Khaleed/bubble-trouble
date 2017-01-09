a830a35bbeee9e48fc486db8537e1ce5c88c040a
Khalid Omar Ali <khalid.ali@consultant.com>
2017-01-09 00:42:35 +0000

    Refactor Model and add createBubble constructor

--- app/model.js
+++ app/model.js
@@ -1,26 +1,27 @@
-import  { default as Html } from "./html";
 import { List, Map } from "immutable";
+import { callFirst } from "./helpers";
 
-const canvas = Html.canvas;
+const createBubble = (canvas, vx, vy, color, radius=25, size=2) => {
+    return Map({
+        x: canvas.width/2,
+        y: canvas.height/2,
+        vx: vx,
+        vy: vy,
+        color: color,
+        radius: radius,
+        size: size
+    });
+};
+
+// const createModel = canvas => {
+//     // return Model based on canvas
+//     // use createBubble -> canvas
+// };
+
+const createBubbleInCanvas = callFirst(createBubble, canvas);
 
 const Model = Map({
-    bubbles: List.of(Map({
-        x: canvas.width/2,
-        y: canvas.height/2,
-        vx: 100,
-        vy: 200,
-        color: "red",
-        radius: 25,
-        size: 2
-    }), Map({
-        x: canvas.width/2,
-        y: canvas.height/2,
-        vx: -100,
-        vy: 200,
-        color: "green",
-        radius: 25,
-        size: 2
-    })),
+    bubbles: List.of(createBubbleInCanvas(100, 200, "red"), createBubbleInCanvas(-100, 200, "green")),
     player: Map({
         x: canvas.width/2,
         w: 20,
@@ -28,30 +29,29 @@ const Model = Map({
         y: canvas.height - 40,
         color: "blue"
     }),
-    arrows: List.of()
+    arrows: List.of(),
+    standardBubbles: List.of(
+        Map({
+            // size === 0
+            vy_init: -200,
+            radius: 10
+        }),
+        Map({
+            // size === 1
+            vy_init: -300,
+            radius: 20
+        }),
+        Map({
+            // size === 2
+            vy_init: -400,
+            radius: 30
+        }),
+        Map({
+            // size === 3
+            vy_init: -550,
+            radius: 45
+        })
+    )
 });
 
-const standardBubbles = List.of(
-    Map({
-        // size === 0
-        vy_init: -200,
-        radius: 10
-    }),
-    Map({
-        // size === 1
-        vy_init: -300,
-        radius: 20
-    }),
-    Map({
-        // size === 2
-        vy_init: -400,
-        radius: 30
-    }),
-    Map({
-        // size === 3
-        vy_init: -550,
-        radius: 45
-    })
-);
-
-export { Model, standardBubbles };
+export { Model };
