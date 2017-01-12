39bfdb3a89274fd46811ecd542c5181733e6a38d
Khalid Omar Ali <khalid.ali@consultant.com>
2017-01-12 18:17:19 +0000

    Separate standard bubbles data from the game model

--- app/model.js
+++ app/model.js
@@ -4,7 +4,6 @@ import { createBubble } from "./helpers";
 const newModel = canvas => {
     const midX = canvas.width/2;
     const midY = canvas.height/2;
-
     return Map({
         bubbles: List.of(createBubble(midX, midY, 100, 200, "red"), createBubble(midX, midY, -100, 200, "green")),
         player: Map({
@@ -15,29 +14,31 @@ const newModel = canvas => {
             color: "blue"
         }),
         arrows: List.of(),
-        isGameOver: false,
-        standardBubbles: List.of(
-            Map({
-                // size === 0
-                vy_init: -200,
-                radius: 10
-            }),
-            Map({
-                // size === 1
-                vy_init: -300,
-                radius: 20
-            }),
-            Map({
-                // size === 2
-                vy_init: -400,
-                radius: 30
-            }),
-            Map({
-                // size === 3
-                vy_init: -550,
-                radius: 45
-            })
-        )});
+        isGameOver: false
+    });
 };
 
-export { newModel };
+const standardBubbles = List.of(
+    Map({
+        // size === 0
+        vy_init: -200,
+        radius: 10
+    }),
+    Map({
+        // size === 1
+        vy_init: -300,
+        radius: 20
+    }),
+    Map({
+        // size === 2
+        vy_init: -400,
+        radius: 30
+    }),
+    Map({
+        // size === 3
+        vy_init: -550,
+        radius: 45
+    })
+);
+
+export { newModel, standardBubbles };
