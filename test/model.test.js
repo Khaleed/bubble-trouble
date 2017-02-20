12fbbae9bb7e2b4a8ad57ef8ff3ff3fe5e5b1e86
Khalid Omar Ali <khalid.ali@consultant.com>
2017-02-20 21:26:23 +0000

    Testing the arrows and bubbles collision detection

--- test/model.test.js
+++ test/model.test.js
@@ -1,8 +1,28 @@
 /*eslint fp/no-unused-expression: 0, fp/no-nil: 0 */
-import sum from "./sum";
+import { isRectStrikingBubble } from "../app/update";
+import { List, Map } from "immutable";
 
-describe("it is a function that adds ", () => {
-    it("adds 1 and 2", () => {
-        expect(sum(1, 2)).toBe(3);
+const bubbles = List.of(Map({
+    x: 100,
+    y: 200,
+    radius: 25
+}), Map({
+    x: 100,
+    y: 400,
+    radius: 25
+}));
+
+const arrows = List.of(
+    Map({
+        x: 100,
+        y: 600,
+        w: 3
+    })
+);
+
+describe("it is a function that tests if arrow and bubble are colliding ", () => {
+    it("it returns a bool", () => {
+        expect(isRectStrikingBubble(bubbles.get(0), arrows.get(0))).toBe(false);
+        expect(isRectStrikingBubble(bubbles.get(1), arrows.get(0))).toBe(true);
     });
 });
